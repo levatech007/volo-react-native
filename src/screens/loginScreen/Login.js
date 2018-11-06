8c451cedb10dc398aec3cf835bb3beb28e3b0b29
Eva <evaliisagalen@gmail.com>
2018-11-05 16:51:59 -0800

    Created Login Screen with styles

---
+++ src/screens/loginScreen/Login.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import { View, TextInput, Button, Alert, Text, TouchableOpacity } from 'react-native';
+import { loginStyles } from './LoginStyles';
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.logUserIn = this.logUserIn.bind(this);
+  }
+
+  logUserIn() {
+    Alert.alert(this.state.password + ' ' + this.state.email)
+
+  }
+
+  render() {
+    return(
+      <View style={loginStyles.container}>
+        <TextInput
+          style={loginStyles.textInput}
+          placeholder='Email'
+          onChangeText={(email) => this.setState({email})}
+          value={this.state.email}
+          autoCapitalize = 'none'
+        />
+        <TextInput
+          style={loginStyles.textInput}
+          placeholder='Password'
+          onChangeText={(password) => this.setState({password})}
+          value={this.state.password}
+          autoCapitalize='none'
+          secureTextEntry={true}
+        />
+        <TouchableOpacity
+          style={ loginStyles.submitButton }
+          onPress = { this.logUserIn}>
+          <Text style={loginStyles.submitButtonText}>Submit</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+export default Login;
---
+++ src/screens/loginScreen/LoginStyles.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+const loginStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#00f260',
+    justifyContent: 'center',
+  },
+  textInput: {
+    margin: 15,
+    height: 40,
+    borderBottomColor: 'gray',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+  },
+  submitButton: {
+      backgroundColor: 'gray',
+      borderRadius: 3,
+      padding: 10,
+      margin: 15,
+      height: 40,
+      alignItems: 'center',
+   },
+   submitButtonText:{
+      color: 'white'
+   }
+});
+
+export { loginStyles }
