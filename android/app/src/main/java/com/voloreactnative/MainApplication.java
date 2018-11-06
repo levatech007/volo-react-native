91ac37b66b3f98d08ac96ee1a752bf86a410ec25
Eva <evaliisagalen@gmail.com>
2018-11-05 22:01:23 -0800

    fixed error CFBundleIdentifier Does not Exist w/ $ react-native upgrade

--- android/app/src/main/java/com/voloreactnative/MainApplication.java
+++ android/app/src/main/java/com/voloreactnative/MainApplication.java
@@ -3,8 +3,6 @@ package com.voloreactnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.q42.rngradient.GradientPackage;
-import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new GradientPackage(),
-            new LinearGradientPackage()
+          new MainReactPackage()
       );
     }
 
