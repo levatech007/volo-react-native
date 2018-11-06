e6b94f915e0fc4011a0a2aaa80e00ce09710ae63
Eva <evaliisagalen@gmail.com>
2018-11-05 16:52:32 -0800

    added Login screen

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,8 +1,12 @@
 import { createStackNavigator } from 'react-navigation';
 import Home from './src/screens/homeScreen/Home';
+import Login from './src/screens/loginScreen/Login';
+
+
 
 const AppNavigator = createStackNavigator({
   Home: { screen: Home },
+  Login: { screen: Login }
 });
 
 export default AppNavigator;
